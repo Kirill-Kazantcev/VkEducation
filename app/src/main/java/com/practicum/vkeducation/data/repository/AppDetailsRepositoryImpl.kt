f0acdaf53a7af0072c0d6f5af17c3404f860561e
Kinza <kira-kazan1997@yandex.ru>
2026-04-16 03:17:41 +0300

    feat: add fallback om mock

--- app/src/main/java/com/practicum/vkeducation/data/repository/AppDetailsRepositoryImpl.kt
+++ app/src/main/java/com/practicum/vkeducation/data/repository/AppDetailsRepositoryImpl.kt
@@ -1,16 +1,26 @@
 package com.practicum.vkeducation.data.repository
 
 import com.practicum.vkeducation.data.api.AppApi
+import com.practicum.vkeducation.data.mock.AppDetailsMockApi
 import com.practicum.vkeducation.data.mapper.toDomain
 import com.practicum.vkeducation.domain.appdetails.AppDetails
 import com.practicum.vkeducation.domain.repository.AppDetailsRepository
+import kotlinx.coroutines.delay
 import javax.inject.Inject
 
 class AppDetailsRepositoryImpl @Inject constructor(
     private val appApi: AppApi
 ) : AppDetailsRepository {
+
     override suspend fun getAppDetails(id: String): AppDetails {
-        val dto = appApi.getAppDetails(id)
-        return dto.toDomain()
+        return try {
+            val dto = appApi.getAppDetails(id)
+            dto.toDomain()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            delay(500)
+            val mockDto = AppDetailsMockApi.getAppDetails().copy(id = id)
+            mockDto.toDomain()
+        }
     }
 }
